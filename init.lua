71c277bc313f93ac09ae80e9f157b9b026e1e8ca
Coaxgames <57080080+Coaxgames@users.noreply.github.com>
2025-08-30 18:57:11 -0600

    Updated to safeAPI
    Not using lua IO system anymore (it is clunky ah)
    Setting and mod are now minimal, Load order detection may be added. but just load the mod above wand_dbg

--- init.lua
+++ init.lua
@@ -1,26 +1,15 @@
-function OnModPreInit()
-    local patchEnabled = ModSettingGet("wdbg_patch.enabledpatch")
-    local originalScriptPath = "mods/wand_dbg/files/debugger.lua"
-    local patchedScriptPath = "mods/wdbg_patch/patchedv1.lua"
-
-    if patchEnabled then
-        -- Apply the patch by setting the mod setting to use the patched script
-        local Content = ModTextFileGetContent(patchedScriptPath)
-        ModTextFileSetContent(originalScriptPath, Content)
-        print("wdbg Patch applied!")
-        --GamePrint("Patch applied. Please restart the game for changes to take effect.")
-    else
-        -- Dont Patch the Script
-        --ModSettingSet("wand_dbg.script_path", originalScriptPath)
-        print("No Patch applied!")
-    end
+if ModSettingGet("wdbg_patch.enabledpatch") then
+    dofile("mods/wdbg_patch/files/patcher.lua")
 end
 
+local done=false
 function OnWorldPreUpdate()
-    if GameGetFrameNum() % 120 == 0 then
+    if GameGetFrameNum() % 120 == 0 and not done then
         if ModSettingGet("wdbg_patch.enabledpatch") then
             print("Patch Enabled!")
+        else
+            print("Patch Disabled!")
         end
-        --GamePrint("Patch state changed. Please restart the game for changes to take effect.")
+        done = true
     end
 end
